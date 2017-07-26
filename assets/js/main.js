28e24e4621e910a73c7670004eaad404400bd7a1
Mariley20 <mariley2018@gmail.com>
2017-07-26 10:06:39 -0500

    creando  la clase todolist metodo agregar

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,19 @@ function Tarea(id, titulo){
 		return html;
 	}
 };
+function ToDoList(){
+	this. list = [];
+	this.id = 0;
+	this.tareaNueva = document.getElementById('tareaNueva');
+	this.agregarTarea = function(){
+		//alert(this.tareaNueva.value)
+		if(this.tareaNueva.value != ""){
+			this.list.push(new Tarea(this.id, this.tareaNueva.value));
+			console.log(this.list)
+			this.id++;
+			toDoList.mostrarLista();
+		}else{
+			alert("ingrese tarea")
+		}
+	}
+}
